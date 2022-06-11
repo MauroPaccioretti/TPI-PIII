2b930ec5419de205afaf779b2fd78272889f053f
Mauro <mauropaccioretti@gmail.com>
2022-06-11 14:47:50 -0300

    Fix FK bug on Person Entity

--- tpi/Migrations/20220611173831_name-change.Designer.cs
+++ tpi/Migrations/20220611173831_name-change.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using tpi.DBContexts;
 
@@ -10,9 +11,10 @@ using tpi.DBContexts;
 namespace tpi.Migrations
 {
     [DbContext(typeof(AppTPIContext))]
-    partial class AppTPIContextModelSnapshot : ModelSnapshot
+    [Migration("20220611173831_name-change")]
+    partial class namechange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.5");
