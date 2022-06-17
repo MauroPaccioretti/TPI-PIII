9ffc98c0bba5b61ef94524001cbbe594513f2c26
brunofruet <brunofruet17@gmail.com>
2022-06-17 19:25:56 -0300

    Model and Entity created

---
+++ tpi/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tpi.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Role { get; set; }
+
+        [ForeignKey("PersonId")]
+        public Person? Person { get; set; }
+
+        [ForeignKey("PersonTypeId")]
+        public PersonType? PersonType { get; set; }
+
+        public User(string name, string role, int id)
+        {
+            Name = name.Trim();
+            Role = role;
+            Id = id;
+        }
+    }
+}
---
+++ tpi/Models/UserToCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tpi.Models
+{
+    public class UserToCreateDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [Required]
+        public string Id { get; set; }
+
+        public UserToCreateDTO(string name, string role, string id)
+        {
+            Name = name;
+            Role = role;
+            Id = id;
+        }
+    }
+}
